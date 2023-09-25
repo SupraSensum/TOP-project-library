9f7278703e0438ce37491e329397a34fcf1845b6
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-09-25 23:57:19 +0000

    Layout the basics

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(author, title, numPages, read = false, yearPublished) {
+   this.author = author;
+   this.title = title;
+   this.numPages = numPages;
+   this.read = read;
+   this.yearPublished = yearPublished;
+}
+
+function addBookToLibrary() {
+   
+}
